f07bbc7976f1f7eceac722e6e87b6fe33661764a
pfernandes21 <pedro.miguel.fernandes@tecnico.ulisboa.pt>
2021-03-02 21:27:04 +0000

    chenged vuex state location to localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import createPersistedState from 'vuex-persistedstate'
-import * as Cookies from 'js-cookie'
+// import * as Cookies from 'js-cookie'
 
 import { auth } from './auth.module';
 
@@ -11,11 +11,14 @@ export default new Vuex.Store({
   modules: {
     auth
   },
-  plugins: [
-    createPersistedState({
-      getState: (key) => Cookies.getJSON(key),
-      setState: (key, state) => Cookies.set(key, state, { expires: 1, secure: true }),
-      removeItem: key => Cookies.remove(key)
-    })
-  ]
+  // plugins: [
+  //   createPersistedState({
+  //     getState: (key) => Cookies.getJSON(key),
+  //     setState: (key, state) => Cookies.set(key, state, { expires: 1, secure: true }),
+  //     removeItem: key => Cookies.remove(key)
+  //   })
+  // ]
+  plugins: [createPersistedState({
+    storage: window.sessionStorage,
+  })],
 });
